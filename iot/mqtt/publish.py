dfe25de5fcca382fff26d8c2f9ea04a2aacb42c6
lehphuc2002 <lehuuphucc2002@gmail.com>
2024-05-27 13:48:32 +0700

    add_2_sensors_test

--- iot/mqtt/publish.py
+++ iot/mqtt/publish.py
@@ -5,10 +5,12 @@ import paho.mqtt.client as paho
 from datetime import datetime
 
 ACCESS_TOKEN1 = 'CAR1_TOKEN'
-broker = '192.168.31.222'
-port = 1883
-longitude_GPS = 10.8805691
-latitude_GPS = 106.8002365
+broker = '0.tcp.ap.ngrok.io'
+port = 15818
+#broker = '192.168.1.4'
+#port = 1883
+longitude_GPS = 106.65829755
+latitude_GPS = 10.771835167
 
 def on_publish(client, userdata, result):
 	print("Data published to ThingsBoard")
@@ -23,7 +25,7 @@ def on_log(client, userdata, level, buf):
 	print("log: ", buf)
 
 def get_temperature():
-	return rnd.uniform(-50, 50)
+	return round(rnd.uniform(-50, 50), 2)
 
 def get_longitude_GPS():
 	return longitude_GPS
@@ -32,10 +34,10 @@ def get_latitude_GPS():
 	return latitude_GPS
 
 def get_humidity():
-	return rnd.uniform(0, 100)
+	return round(rnd.uniform(0, 100),2)
 
 def get_wind_direction():
-	return rnd.randrange(0, 360)
+	return round(rnd.randrange(0, 360),2)
 
 def get_payload():
 	now = datetime.now()
@@ -57,7 +59,7 @@ client1.on_log = on_log
 client1.username_pw_set(ACCESS_TOKEN1)
 
 try:
-	client1.connect(broker, port, keepalive=5)
+	client1.connect(broker, port, keepalive=60)
 except Exception as e:
 	print(f"Could not connect to MQTT Broker. Error: {e}")
 	exit()
@@ -69,6 +71,8 @@ try:
 		time.sleep(0.1)
 
 	while True:
+		latitude_GPS += rnd.uniform(-0.0001, 0.0001)
+		longitude_GPS += rnd.uniform(-0.0001, 0.0001)
 		payload1 = get_payload()
 		ret = client1.publish("v1/devices/me/telemetry", payload1)
 		if ret.rc == paho.MQTT_ERR_SUCCESS:
