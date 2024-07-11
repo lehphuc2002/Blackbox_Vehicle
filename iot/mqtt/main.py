c19f591492e41eb2b1596ba27cecdbaca232796f
hoannaoh <lethehoanhug02@gmail.com>
2024-07-11 13:41:46 +0700

    Changed

--- iot/mqtt/main.py
+++ iot/mqtt/main.py
@@ -21,61 +21,103 @@
 #  MA 02110-1301, USA.
 #  
 #  
-from publish import init_client, get_payload, ACCESS_TOKEN1, ACCESS_TOKEN2
+
+from publish import init_client, get_payload_test, ACCESS_TOKEN1, ACCESS_TOKEN2,ACCESS_TOKEN3, send_data_test, get_payload,ACC_X_THRESHOLD,ACC_Y_THRESHOLD ,ACC_Z_THRESHOLD, haversine, start_time_latency
 import paho.mqtt.client as paho
 from BNO055_lib import BNO055Sensor
-from GPS_lib import GPSModule
+from GPS_lib import GPSReader
 import time
 import math
 from RFID_lib import RFIDReader, create_user_library
 global user_info
-import distant
+import serial
+from datetime import datetime
+import threading
+import os
+import json
+import publish_test
 
+def start_recording(filename):
+	print("start record")
+	os.system(f"rpicam-vid -t 3000s -o /home/pi/Videos{filename}")
+	time.sleep(2)  # Start recording
+
+
+def stop_recording():
+	print("Stop record")
+	os.system("sudo pkill rpicam-vid")
+    
 def main():
 	#  MPU, GPS, client init
+	global status, start_time_latency
+	print("Connect to internet or reset module 4G for system")
+	#time.sleep(60)
+	
 	mpu_BNO055 = BNO055Sensor()
-	gps_EM06 = GPSModule()
+	gps = GPSReader()
 	client1 = init_client(ACCESS_TOKEN2)
+	global ser1
+	ser1 = serial.Serial("/dev/ttyUSB1", 115200)
 	user_library = create_user_library()
+
 	reader = RFIDReader(user_library)
+	time_lt = time.time()
 	user_info = dict({'name': 'Chua Quet The', 'phone': 'None'})
 	dt = 0
-	time_start = time.time()
+	d_t = 1
+	last_send_time = time.time()
 	vx = 0
 	vy = 0
 	vz = 0
 	v = 0
 	acc_offset = mpu_BNO055.accel_calib()
-	longitude_GPS_t, latitude_GPS_t = gps_EM06.read_coordinates()
+	
+
 	try:
 		
+		gps.start()
+		now = datetime.now()
+		filename = now.strftime("%Y%m%d_%H%M%S.h264")
+
+    # Create and start the recording thread
+		recording_thread = threading.Thread(target=start_recording, args=(filename,))
+		recording_thread.start()
+		longitude_GPS_t, latitude_GPS_t = gps.get_current_location()
+		print("Recording started")
+		time.sleep(2)
 		while True:
 			#  Read acceleration
 			ax, ay, az = mpu_BNO055.read_sensor_data()
-			print("Acclerometer", ax,ay,az)
+			
 			ax = ax - acc_offset[0]
 			ay = ay - acc_offset[1]
 			az = az - acc_offset[2]
-			
-			t = time.time()
-			dt = t-time_start
-			time_start = t
+			print("Acclerometer", ax,ay,az)
+			status = 'Normal'
+			current_time = time.time()
+			dt = current_time - last_send_time
 
-			vx = vx + round(ax,1)*dt
-			vy = vy + round(ay,1)*dt
-			vz = vz + round(az,1)*dt
+
+			vx = vx + round(ax,0)*dt
+			vy = vy + round(ay,0)*dt
+			vz = vz + round(az,0)*dt
 			
-			v = math.sqrt(vx*vx +vy*vy +vz*vz)*3.6
+			v_1 = math.sqrt(vx*vx +vy*vy +vz*vz)*3.6
+			v = v_1
 			#  Read GPS parameters
-			longitude_GPS, latitude_GPS = gps_EM06.read_coordinates()
-			if longitude_GPS is not None and latitude_GPS is not None and longitude_GPS_t is not None and latitude_GPS_t is not None :
-				print(f"Longitude: {longitude_GPS}, Latitude: {latitude_GPS}")
-				d = gps_EM06.haversine(longitude_GPS_t, latitude_GPS_t, longitude_GPS, latitude_GPS)
-				v_2 = d/dt
-				v = 0.999*v_2 + 0.001*v_1
+			latitude_GPS, longitude_GPS = gps.get_current_location()
+			print(f"Longitude: {longitude_GPS}, Latitude: {latitude_GPS}")
+			# if longitude_GPS is not None and latitude_GPS is not None and longitude_GPS_t is not None and latitude_GPS_t is not None :
+				# time_t = time.time()
+				# d_t = time_t - time_lt
+				# d = haversine(longitude_GPS_t, latitude_GPS_t, longitude_GPS, latitude_GPS)
+				# v_2 = d/d_t
+				# time_lt = time_t
+				# v = v_2/3.6
+				
+			if v > 90:
+				status = 'Over Speed'
 			
-			
-			longitude_GPS_t, latitude_GPS_t = longitude_GPS, latitude_GPS
 			print("velocity", v)
 			uid = reader.read_id()
 			if uid is not None:
@@ -86,22 +128,62 @@ def main():
 					print(f'UID {uid:X} not found in user library.')
 
 			
-			payload1 = get_payload(ax, ay, az, v, user_info["name"], user_info["phone"],longitude_GPS , latitude_GPS)
-			ret = client1.publish("v1/devices/me/telemetry", payload1)
 			
-			if ret.rc == paho.MQTT_ERR_SUCCESS:
-				print("Publish success")
-				print("Here is the latest telemetry")
-				print(payload1)
-			else:
-				print(f"Publish failed with error code: {ret.rc}")
-			time.sleep(1)
+			
+			if ax > ACC_X_THRESHOLD or ay > ACC_Y_THRESHOLD or az > ACC_Z_THRESHOLD:
+				status = 'Warning Accident'
+			#	payload1 = get_payload_test(ax, ay, az, speed, status)
+				ret = client1.publish("v1/devices/me/telemetry", payload1)
+				if ret.rc == paho.MQTT_ERR_SUCCESS:
+					print("Immediate alert published")
+					print(payload1)
+				else:
+					print(f"Immediate alert failed with error code: {ret.rc}")
+					
+				rpc_request = {'method': 'getCurrentTime', 'params': {} }
+				publish_test.start_time_latency = time.time()
+				request_id = 1 
+				client1.publish('v1/devices/me/rpc/request/' + str(request_id), json.dumps(rpc_request))
+                #~~~~~###################### end test latency ----	
+			# Doi khoang 3s moi gui
+			if current_time - last_send_time >= 3:
+			#	payload1 = get_payload_test(ax, ay, az, speed, status)
+				
+				if longitude_GPS is not None and latitude_GPS is not None and longitude_GPS_t is not None and latitude_GPS_t is not None :
+					d_t = current_time - last_send_time
+					d = haversine(longitude_GPS_t, latitude_GPS_t, longitude_GPS, latitude_GPS)
+					v_2 = d/d_t
+					v = v_2*3.6
+				longitude_GPS_t, latitude_GPS_t = longitude_GPS, latitude_GPS
+				payload1 = get_payload(ax, ay, az, v, user_info["name"], user_info["phone"],longitude_GPS , latitude_GPS, status)
+				ret = client1.publish("v1/devices/me/telemetry", payload1)
+				if ret.rc == paho.MQTT_ERR_SUCCESS:
+					print("Regular data published")
+					print(payload1)
+				else:
+					print(f"Regular data failed with error code: {ret.rc}")
+				rpc_request = {'method': 'getCurrentTime', 'params': {} }
+				publish_test.start_time_latency = time.time()
+				request_id = 1 
+				client1.publish('v1/devices/me/rpc/request/' + str(request_id), json.dumps(rpc_request))
+				last_send_time = current_time
+				
+			# if ret.rc == paho.MQTT_ERR_SUCCESS:
+				# print("Publish success")
+				# print("Here is the latest telemetry")
+				# print(payload1)
+			# else:
+				# print(f"Publish failed with error code: {ret.rc}")
+			time.sleep(0.1)
 	except KeyboardInterrupt:
 		print("Disconnecting from MQTT Broker...")
 		client1.disconnect()
 		client1.loop_stop()
 		print("Disconnected")
-		gps_EM06.destroy()
+		gps.stop() 
+		gps.destroy()
+		print("Program terminated.")
+		stop_recording()
 	
 	return 0
 
